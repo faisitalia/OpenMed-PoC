b33ab28de4d1996043d4f7c7465f6df214edadb3
Michele Sciabarra <michele@sciabarra.com>
2021-10-05 13:48:38 +0200

    prototype ui

---
+++ proto/src/App.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+	function ciao() { alert("ciao")}
+</script>
+
+<ons-page id="helloworld-page">
+	<ons-toolbar>
+		<div class="center" />
+		<div class="right">
+			<ons-toolbar-button >
+				<ons-icon icon="ion-ios-menu, material:md-menu" />
+			</ons-toolbar-button>
+		</div>
+	</ons-toolbar>
+
+	<p style="text-align: center">
+		<ons-button on:click={ciao}>Click me!</ons-button>
+	</p>
+</ons-page>
