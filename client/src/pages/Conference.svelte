268753a128a6e2c4ad22bcdf83469513aa9f1d3b
Michele Sciabarra <michele@sciabarra.com>
2021-10-24 12:05:23 +0200

    removed sample

--- client/src/pages/Conference.svelte
+++ client/src/pages/Conference.svelte
@@ -42,71 +42,79 @@
         </div>
     {:else}
         <!-- top -->
-        <video
-            id="producer-video0"
-            class="w-full h-2/3 border border-red"
-            autoPlay
-            playsInline
-            muted
-            controls={false}
-        />
-        <audio
-            id="producer-audio0"
-            autoPlay
-            playsInline
-            muted={false}
-            controls={false}
-        />
+        <div class="w-full h-full border border-red">
+            <video
+                class="w-full h-full object-cover"
+                id="consumer-video0"
+                autoPlay
+                playsInline
+                muted
+                controls={false}
+            />
+            <audio
+                id="consumer-audio0"
+                autoPlay
+                playsInline
+                muted={false}
+                controls={false}
+            />
+        </div>
         <div class="absolute top-0 left-0 badge badge-primary">{roomId}y</div>
         <div class="absolute top-0 right-0 badge badge-secondary">{peerId}</div>
 
         <!-- first -->
-        <video
-            id="consumer-video1"
-            class="absolute bottom-0 left-0 w-1/3 h-1/3 border border-red"
-            autoPlay
-            playsInline
-            muted
-            controls={false}
-        />
-        <audio
-            id="consumer-audio1"
-            autoPlay
-            playsInline
-            muted={false}
-            controls={false}
-        />
+        <div class="absolute bottom-0 left-0 w-1/3 h-1/3 border border-red">
+            <video
+                id="producer-video0"
+                class="w-full h-full object-cover"
+                autoPlay
+                playsInline
+                muted
+                controls={false}
+            />
+            <audio
+                id="producer-audio0"
+                autoPlay
+                playsInline
+                muted={false}
+                controls={false}
+            />
+        </div>
         <!-- second -->
-        <video
-            id="consumer-video2"
-            class="absolute bottom-0 left-1/3 w-1/3 h-1/3 border border-red"
-            autoPlay
-            playsInline
-            muted
-            controls={false}
-        />
-        <audio
-            id="consumer-audio2"
-            autoPlay
-            playsInline
-            muted={false}
-            controls={false}
-        />
+        <div class="absolute bottom-0 left-1/3 w-1/3 h-1/3 border border-red">
+            <video
+                id="consumer-video1"
+                class="w-full h-full object-cover"
+                autoPlay
+                playsInline
+                muted
+                controls={false}
+            />
+            <audio
+                id="consumer-audio1"
+                autoPlay
+                playsInline
+                muted={false}
+                controls={false}
+            />
+        </div>
         <!-- third -->
-        <video
-            id="consumer-video3"
-            class="absolute bottom-0 left-2/3 w-1/3 h-1/3 border border-red"
-            autoPlay
-            playsInline
-            muted
-            controls={false}
-        />
-        <audio
-            id="consumer-audio3"
-            autoPlay
-            playsInline
-            muted={false}
-            controls={false}
-        />
-        #{/if}
+        <div class="absolute bottom-0 left-2/3 w-1/3 h-1/3 border border-red">
+            <video
+                id="consumer-video3"
+                class="w-full h-full object-cover"
+                autoPlay
+                playsInline
+                muted
+                controls={false}
+            />
+            <audio
+                id="consumer-audio3"
+                autoPlay
+                playsInline
+                muted={false}
+                controls={false}
+            />
+        </div>
+        {/if}
 </div>
