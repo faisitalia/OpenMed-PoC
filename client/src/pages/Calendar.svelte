e66c0e824d14b357d94aea6b093ae2bdfd4da470
Michele Sciabarra <michele@sciabarra.com>
2021-10-28 11:51:50 +0200

    missing files

---
+++ client/src/pages/Calendar.svelte
@@ -0,0 +1,19 @@
+<script>
+    import { get, post, del } from "../util";
+</script>
+
+{#await get("/schedules") then schedules}
+    {#each schedules as sched}
+        <a
+            href=""
+            class="block p-6 bg-white hover:bg-gray-100 shadow-md border border-gray-200 rounded-lg max-w-sm"
+        >
+            <h5 class="text-gray-900 font-bold text-2xl tracking-tight mb-2">
+                Appuntamento con <br />{sched.paziente}
+            </h5>
+            <p class="font-normal text-gray-700">
+                il {sched.data} alle {sched.ora}
+            </p>
+        </a>
+    {/each}
+{/await}
