06b69738645c62212b98f0bf491f189f6473a075
Mirella Di Girolamo <mirella@sciabarra.com>
2021-11-07 23:02:14 +0100

    form validation partial

--- client/src/pages/Schedule.svelte
+++ client/src/pages/Schedule.svelte
@@ -2,6 +2,78 @@
     import { get, post, del } from "../util";
     import Datepicker from "@themesberg/tailwind-datepicker/Datepicker";
     import { onMount } from "svelte";
+    
+    import validate from "validate.js";
+    validate.validators.presence.message = "Non può essere vuoto"
+
+    import moment from "moment";
+    window["moment"]=moment;
+   
+
+    // Hook up the form so we can prevent it from being posted
+    let form = {};
+    let errors = {};
+    onMount(() => (form = document.querySelector("form#main")));
+
+    function error(map, name) {
+        if (name in map) {
+            //document.getElementById(name).classList.add("text-red")
+            let label = document.querySelector("label[for='" + name + "']")
+            if(label)
+              label.classList.add("text-red-500")
+
+            return map[name].join("<br>");
+        } else {
+            let label = document.querySelector("label[for='" + name + "']")
+            if(label)
+              label.classList.remove("text-red-500")
+            //document.getElementById(name).classList.remove("text-red")
+        }
+        return "";
+    }
+
+    // Before using it we must add the parse and format functions
+    // Here is a sample implementation using moment.js
+    validate.extend(validate.validators.datetime, {
+        // The value is guaranteed not to be null or undefined but otherwise it
+        // could be anything.
+        parse: function (value, options) {
+            return +moment.utc(value);
+        },
+        // Input is a unix timestamp
+        format: function (value, options) {
+            var format = options.dateOnly
+                ? "YYYY-MM-DD"
+                : "YYYY-MM-DD hh:mm:ss";
+            return moment.utc(value).format(format);
+        },
+    });
+    // These are the constraints used to validate the form
+    var constraints = {
+        
+        scheduledata: {
+            // The user needs to give a valide schedule data
+            presence: true,
+            // and must be after now
+            date: {
+                earliest: moment().add(1, "days"),
+                message:
+                    "Almeno domani",
+            },
+        },
+        
+    };
+
+    function submit(event) {
+        // validate the form against the constraints
+        errors = validate(form, constraints);
+        if (!errors) {
+            //alert("success");
+            save(event);
+        } else {
+            console.log("errors", errors);
+        }
+    }
 
     let sent = false;
 
@@ -58,16 +130,13 @@
     function save(event) {
         event.preventDefault();
         data.ora = selectedH + ":" + selectedM;
-        data.data = document.getElementById("datepicker").value;
+        data.data = document.getElementById("scheduledata").value;
         console.log(data);
         post("/schedule", data);
         sent = true;
     }
 
-    onMount(() => {
-        const datepickerEl = document.getElementById("datepicker");
-        new Datepicker(datepickerEl, {});
-    });
+    
 </script>
 
 <div class="p-3">
@@ -105,7 +174,7 @@
             <div class="card-header">
                 <h1 class="text-black text-lg">Nuovo appuntamento</h1>
             </div>
-            <form>
+            <form id="main">
                 <div class="form-control">
                     <!-- svelte-ignore a11y-label-has-associated-control -->
                     <label class="label">
@@ -113,16 +182,15 @@
                             >Seleziona una data</span
                         >
                     </label>
-
                     <input
-                        name="datepicker"
-                        id="datepicker"
-                        datepicker-title="Flowbite datepicker"
-                        datepicker-format="dd/mm/yyyy"
-                        type="text"
-                        class="input input-accent input-bordered w-1/3"
-                        placeholder="Seleziona il giorno"
-                    />
+                    id="scheduledata"
+                    class="form-control input input-accent input-bordered w-1/3"
+                    type="date"
+                    placeholder="YYYY-MM-DD"
+                    name="scheduledata"
+                />
+                <div class="col-sm-5 messages">{error(errors, "scheduledata")}</div>
+                    
                 </div>
                 <div class="form-control w-1/2">
                     <!-- svelte-ignore a11y-label-has-associated-control -->
@@ -194,7 +262,7 @@
                 <div class="form-control">
                     <button
                         class="btn btn-accent content-center max-w-xs"
-                        on:click={save}
+                        on:click|preventDefault={submit}
                         color="primary"
                         block
                         href="pages/authentication/login">Prenota</button
